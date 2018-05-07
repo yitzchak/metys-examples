4a533da61ae2755f25a2b9e98b5c368260a174ee
Tarn Burton <twburton@gmail.com>
2018-05-07 06:56:59 -0400

    Update for character based modes

--- minimal-maxima/maxima-init.lisp
+++ minimal-maxima/maxima-init.lisp
@@ -1,4 +1,4 @@
 (load "../../maxima-packages/texify/texify.lisp")
 
 (defun tex (x l r lop rop)
-  (append l (list (texify x nil '(0) lop rop)) r))
+  (append l (list (texify x nil '(#\m) lop rop)) r))
