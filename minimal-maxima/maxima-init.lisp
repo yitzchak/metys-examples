bc01cec1825b8be10c285ab574097c724ed90849
Tarn Burton <twburton@gmail.com>
2018-05-14 13:56:52 -0400

    Use breqn

--- minimal-maxima/maxima-init.lisp
+++ minimal-maxima/maxima-init.lisp
@@ -1,4 +1,4 @@
 (load "../../maxima-packages/texify/texify.lisp")
 
 (defun tex (x l r lop rop)
-  (append l (list (texify x nil '(#\m) lop rop)) r))
+  (append l (list (texify x '(#\m) lop rop)) r))
